9a166a59369c0d68da7cabf4863ffbb7d10dcef2
Mylena Ferreira de Souza <mylenafds@gmail.com>
2024-06-18 12:31:59 -0300

    Exercicio com typescript

---
+++ src/main.js
@@ -0,0 +1,14 @@
+"use strict";
+// Função de multiplicação
+function multiplicar(a, b) {
+    return a * b;
+}
+// Função de saudação
+function saudar(nome) {
+    return "Olá " + nome;
+}
+// Exemplos de uso das funções
+const resultadoMultiplicacao = multiplicar(5, 3);
+const saudacao = saudar("Gian");
+console.log(resultadoMultiplicacao); // Saída: 15
+console.log(saudacao); // Saída: Olá Gian
